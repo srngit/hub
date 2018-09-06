2bb96d4671dd0a859fdf5d89cbcc9663b6c9ea17
Paul-Hess <pauljacksonhess@gmail.com>
2018-09-06 12:55:44 -0700

    clean up

--- src/test/javascript/stability/rolling_restart_cluster_spec.js
+++ src/test/javascript/stability/rolling_restart_cluster_spec.js
@@ -1,5 +1,4 @@
 const moment = require('moment');
-const rp = require('request-promise-native');
 const {
     closeServer,
     deleteWebhook,
@@ -11,7 +10,6 @@ const {
     hubClientGet,
     hubClientPostTestItem,
     hubClientPut,
-    itSleeps,
     randomChannelName,
     randomString,
     startServer,
@@ -24,23 +22,12 @@ const {
     getChannelUrl,
 } = require('../lib/config');
 
-const {
-    CLUSTER_GROUP,
-    JENKINS_TOKEN,
-    JENKINS_URL,
-    RESTART_ZOOKEEPERS,
-    ZK_TOKEN,
-} = process.env;
-
 const port = getCallBackPort();
 const channelName = randomChannelName();
 const webhookName = randomChannelName();
 const callbackDomain = getCallBackDomain();
 const callbackPath = `/${randomString(5)}`;
 const callbackUrl = `${callbackDomain}:${port}${callbackPath}`;
-// const internalPath = `${getHubUrlBase()}/internal/properties`;
-// const bigString = () => new Array(65536).fill(randomString(16)).join('');
-// const testData = () => new Array(600).fill(bigString());
 const channelResource = `${getChannelUrl()}/${channelName}`;
 const testContext = {
     [channelName]: {
@@ -53,13 +40,9 @@ const testContext = {
 const timeFormat = 'YYYY-MM-DDTHH:mm:ss.SSS';
 const urlTimeFormat = 'YYYY/MM/DD/HH/mm/ss/SSS';
 const mutableTime = moment.utc().subtract(1, 'minute');
-console.log('original mutable time', mutableTime.format());
 const stableMutableTime = moment.utc(mutableTime).subtract(30, 'minutes').format(timeFormat);
-console.log('stableMutableTime', stableMutableTime);
 const startItemTime = moment.utc(mutableTime).subtract(10, 'minutes');
-console.log('startItemTime', startItemTime.format());
 const historicalItemTime = moment.utc(mutableTime).subtract(10, 'seconds');
-console.log('historicalItemTime', historicalItemTime.format());
 const channelBody = {
     mutableTime: mutableTime.format(timeFormat),
 };
@@ -67,11 +50,6 @@ const channelBodyChange = {
     mutableTime: stableMutableTime,
 };
 const headers = { 'Content-Type': 'application/json' };
-// const getZookeepers = (body) => {
-//     const properties = getProp('properties', body) || {};
-//     const zks = properties['zookeeper.connection'];
-//     return zks ? zks.split(',') : [];
-// };
 
 const pendingIfNotReady = () => {
     if (!testContext[channelName].ready) {
@@ -79,56 +57,14 @@ const pendingIfNotReady = () => {
     }
 };
 
-const getMomentFromUrl = (url) => {
-    const arr = (url && url.split('/')) || [];
-    const dateArray = arr && arr.splice(arr.length - 8, 7);
-    return dateArray ? moment.utc(dateArray).subtract(1, 'month') : {};
-};
-
-const fillTimeChunks = (arr, timestamp) => {
-    const fillers = arr.filter(url => {
-        const date = getMomentFromUrl(url) || {};
-        return !!date.valueOf && (date.valueOf() === timestamp);
-    });
-    const urlId = url => (url && (url.split('/') || []).pop());
-    fillers.map(url => url && urlId(url));
-    return fillers || [];
-};
-
-const toTimeChunks = (arr) => {
-    const copy = [...arr];
-    return copy.reduce((accum, key) => {
-        const date = getMomentFromUrl(key) || {};
-        const timestamp = date.valueOf && date.valueOf();
-        if (timestamp && !accum[timestamp]) {
-            accum[timestamp] = fillTimeChunks(copy, timestamp);
-        }
-        return accum;
-    }, {});
-};
-
 describe('stability of webhook delivery during restart of the hub', () => {
     beforeAll(async () => {
         // make a call to the hub to clarify it is alive
         const response1 = await hubClientGet(`${getHubUrlBase()}/channel`);
         const stableStart = getProp('statusCode', response1) === 200;
-        // configure the test based on hub properties
-        // not necessary for single hub
-        // may not be the right path for clustered hub envs
-        // =============begin
-        // const internal = hubClientGet(internalPath, headers);
-        // const properties = getProp('body', internal) || {};
-        // if (RESTART_ZOOKEEPERS) {
-        //     const zookeepers = getZookeepers(properties);
-        //     testContext[channelName].zookeepersToRestart.push(...zookeepers);
-        // }
-        // =============end
-        // create a historical channel
-        // sets mutableTime
+        // create channel
         const response2 = await hubClientPut(channelResource, headers, channelBody);
         const channelStart = getProp('statusCode', response2) === 201;
-
-        // testContext[channelName].callbackServer = response3;
         // tag all as ready to roll
         testContext[channelName].ready = [stableStart, channelStart]
             .every(t => t);
@@ -147,77 +83,15 @@ describe('stability of webhook delivery during restart of the hub', () => {
         testContext[channelName].firstItem = item;
     });
 
-    xit('triggers a rolling restart of the hub cluster', async () => {
-        // pseudo code for clustered hub, zk restarts
-        const JOB = `/job/hub-${CLUSTER_GROUP.replace('.', '-')}-rolling-restart/build`;
-        const jenkinsUrl = `${JENKINS_URL}/view/hub.${CLUSTER_GROUP}${JOB}?token=${JENKINS_TOKEN}`;
-        try {
-            const response = await rp({
-                method: 'POST',
-                resolveWithFullResponse: true,
-                headers,
-                url: jenkinsUrl,
-                json: true,
-            });
-            expect(getProp('statusCode', response)).toEqual(201);
-        } catch (ex) {
-            console.log('ex ', ex && ex.message);
-            fail(ex);
-        }
-        // const {
-        //     zookeepersToRestart,
-        // } = testContext[channelName];
-
-        if (RESTART_ZOOKEEPERS) {
-            const ZK_JOB = '/view/Zookeeper/job/zookeeper-restart/buildWithParameters';
-            const zkUrl = `${JENKINS_URL}${ZK_JOB}?token=${ZK_TOKEN}&CONTEXT=${CLUSTER_GROUP}`;
-            try {
-                const response = await rp({
-                    method: 'POST',
-                    resolveWithFullResponse: true,
-                    headers,
-                    url: zkUrl,
-                    json: true,
-                });
-                expect(getProp('statusCode', response)).toEqual(201);
-            } catch (ex) {
-                console.log('ex ', ex && ex.message);
-                fail(ex);
-            }
-        }
-        // give time for the jobs to get started
-        await itSleeps(10000);
-    });
-
     it('post loads of data after mutableTime', async () => {
         pendingIfNotReady();
-        // const asyncCallback = async () => {
-        //
-        // };
-        // const promises = [1, 2, 3, 4, 5]
-        //     .map(async n => {
-        //         const response = await asyncCallback();
-        //         return response;
-        //     });
-        // const responses = await Promise.all(promises);
         const response = await hubClientPostTestItem(channelResource, headers);
         const item = fromObjectPath(['body', '_links', 'self', 'href'], response);
-        const date = getMomentFromUrl(item);
-        console.log('firstPostTime::::::', date.format());
         testContext[channelName].postedItemHistory.push(item);
     });
 
     it('post loads of data before mutableTime', async () => {
         pendingIfNotReady();
-        // const asyncCallback = async () => {
-        //
-        // };
-        // const promises = [1, 2, 3, 4, 5]
-        //     .map(async n => {
-        //         const response = await asyncCallback();
-        //         return response;
-        //     });
-        // const responses = await Promise.all(promises);
         const pointInThePastURL = `${channelResource}/${historicalItemTime.format(urlTimeFormat)}`;
         const response = await hubClientPostTestItem(pointInThePastURL, headers);
         const item = fromObjectPath(['body', '_links', 'self', 'href'], response);
@@ -261,7 +135,7 @@ describe('stability of webhook delivery during restart of the hub', () => {
         expect(getProp('statusCode', response)).toBe(201);
     });
 
-    it('waits for all the callbacks to happen', async () => {
+    it('waits for all the callbacks to happen (bug documentation?)', async () => {
         pendingIfNotReady();
         const {
             callbackItemHistory,
@@ -271,13 +145,13 @@ describe('stability of webhook delivery during restart of the hub', () => {
             callbackItemHistory.length ===
             postedItemHistory.length
         );
-        await waitForCondition(condition); // times out
+        await waitForCondition(condition); // TODO: times out
         console.log('callbacks made', callbackItemHistory.length);
-        // expect(callbackItemHistory.length).toEqual(postedItemHistory.length); fails
+        // expect(callbackItemHistory.length).toEqual(postedItemHistory.length); TODO: fails
         expect(callbackItemHistory.length).not.toEqual(postedItemHistory.length);
     });
 
-    it('verifies callbacks were made in proper order', () => {
+    it('verifies callbacks were made in proper order (bug documentation?)', () => {
         const {
             callbackItemHistory,
             postedItemHistory,
@@ -289,7 +163,7 @@ describe('stability of webhook delivery during restart of the hub', () => {
             }
             return equal;
         });
-        // expect(actual).toBe(true); fails
+        // expect(actual).toBe(true); TODO: fails
         expect(actual).toBe(false);
     });
 
