b533a0657076b3e4043ed7f305aadc85f965b5d4
Ryan Zander <exide@hotmail.com>
2018-09-11 14:45:07 -0700

    Added some unit tests.

--- src/test/java/com/flightstats/hub/app/HubPropertiesTest.java
+++ src/test/java/com/flightstats/hub/app/HubPropertiesTest.java
@@ -2,6 +2,10 @@ package com.flightstats.hub.app;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -13,6 +17,30 @@ public class HubPropertiesTest {
         HubProperties.setProperty("hub.read.only", "somewhere," + HubHost.getLocalName());
         assertTrue(HubProperties.isReadOnly());
         HubProperties.setProperty("hub.read.only", "");
+    }
+
+    @Test
+    public void testDatadogTagsToIgnoreIsEmpty() {
+        HubProperties.setProperty("data_dog.ignore_tags", "");
+        assertEquals(Collections.emptyList(), HubProperties.getDatadogTagsToIgnore());
+    }
+
+    @Test
+    public void testDatadogTagsToIgnoreIsSingular() {
+        HubProperties.setProperty("data_dog.ignore_tags", "foo");
+        assertEquals(Collections.singletonList("foo:"), HubProperties.getDatadogTagsToIgnore());
+    }
+
+    @Test
+    public void testDatadogTagsToIgnoreIsMultiple() {
+        HubProperties.setProperty("data_dog.ignore_tags", "foo,bar");
+        assertEquals(Arrays.asList("foo:", "bar:"), HubProperties.getDatadogTagsToIgnore());
+    }
+
+    @Test
+    public void testDatadogTagsToIgnoreHasWhitespace() {
+        HubProperties.setProperty("data_dog.ignore_tags", " foo ,  bar,baz, qux ");
+        assertEquals(Arrays.asList("foo:", "bar:", "baz:", "qux:"), HubProperties.getDatadogTagsToIgnore());
     }
 
 }
